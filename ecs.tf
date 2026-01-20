d166a90310cf35418e0e06d35ec5bc3a89cfce44
chanhyeokseo <chanhyeok.seo@datadoghq.com>
2026-01-20 14:54:52 +0900

    fix forwardog

--- ecs.tf
+++ ecs.tf
@@ -25,41 +25,41 @@
 #   value       = module.ecs_fargate.cluster_arn
 # }
 
-# ============================================
-# ECS EC2 Cluster
-# ============================================
-module "ecs_ec2" {
-  source = "./modules/ecs"
+# # ============================================
+# # ECS EC2 Cluster
+# # ============================================
+# module "ecs_ec2" {
+#   source = "./modules/ecs"
 
-  name_prefix    = "${local.project_name_prefix}-ec2"
+#   name_prefix    = "${local.project_name_prefix}-ec2"
 
-  # Fargate Configuration (optional, set to true if you want to use Fargate)
-  enable_fargate = false
-  enable_ec2     = true
+#   # Fargate Configuration (optional, set to true if you want to use Fargate)
+#   enable_fargate = false
+#   enable_ec2     = true
 
-  subnet_ids           = [local.vpc.public_subnet_id, local.vpc.public_subnet2_id]
-  security_group_ids   = [module.security_group.security_group_id]
-  instance_type        = var.ec2_instance_type
-  custom_ami_id        = data.aws_ami.ecs_optimized.id
-  key_name             = var.ec2_key_name
-  ec2_min_size         = 1
-  ec2_max_size         = 3
-  ec2_desired_capacity = 1
+#   subnet_ids           = [local.vpc.public_subnet_id, local.vpc.public_subnet2_id]
+#   security_group_ids   = [module.security_group.security_group_id]
+#   instance_type        = var.ec2_instance_type
+#   custom_ami_id        = data.aws_ami.ecs_optimized.id
+#   key_name             = var.ec2_key_name
+#   ec2_min_size         = 1
+#   ec2_max_size         = 3
+#   ec2_desired_capacity = 1
 
-  common_tags = local.project_common_tags
-}
+#   common_tags = local.project_common_tags
+# }
 
-output "ecs_ec2_cluster_name" {
-  description = "Name of the ECS EC2 cluster"
-  value       = module.ecs_ec2.cluster_name
-}
+# output "ecs_ec2_cluster_name" {
+#   description = "Name of the ECS EC2 cluster"
+#   value       = module.ecs_ec2.cluster_name
+# }
 
-output "ecs_ec2_cluster_arn" {
-  description = "ARN of the ECS EC2 cluster"
-  value       = module.ecs_ec2.cluster_arn
-}
+# output "ecs_ec2_cluster_arn" {
+#   description = "ARN of the ECS EC2 cluster"
+#   value       = module.ecs_ec2.cluster_arn
+# }
 
-output "ecs_ec2_asg_name" {
-  description = "Name of the ECS EC2 Auto Scaling Group"
-  value       = module.ecs_ec2.autoscaling_group_name
-}
+# output "ecs_ec2_asg_name" {
+#   description = "Name of the ECS EC2 Auto Scaling Group"
+#   value       = module.ecs_ec2.autoscaling_group_name
+# }
